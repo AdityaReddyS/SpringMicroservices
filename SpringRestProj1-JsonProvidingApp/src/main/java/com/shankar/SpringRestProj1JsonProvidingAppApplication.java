3e867c7379ea7bd030065fc862bdc1e179f6d470
TL <shankarreddy@LAPTOP-HNPNI8HE>
2023-06-19 13:28:25 +0530

    TL Added a Project to the Central Repository

---
+++ SpringRestProj1-JsonProvidingApp/src/main/java/com/shankar/SpringRestProj1JsonProvidingAppApplication.java
@@ -0,0 +1,13 @@
+package com.shankar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestProj1JsonProvidingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestProj1JsonProvidingAppApplication.class, args);
+	}
+
+}
---
+++ SpringRestProj1-JsonProvidingApp/src/test/java/com/shankar/SpringRestProj1JsonProvidingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestProj1JsonProvidingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
