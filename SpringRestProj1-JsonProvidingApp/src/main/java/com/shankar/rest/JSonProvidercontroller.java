247c71b23b6837307ae0fee0d1f1c928bc98a33a
Dev2@gmail.com <shankarreddy@LAPTOP-HNPNI8HE>
2023-06-19 14:55:18 +0530

    TL added Changes Controller.java

--- SpringRestProj1-JsonProvidingApp/src/main/java/com/shankar/rest/JSonProvidercontroller.java
+++ SpringRestProj1-JsonProvidingApp/src/main/java/com/shankar/rest/JSonProvidercontroller.java
@@ -1,5 +1,20 @@
 package com.shankar.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shankar.model.Customer;
+
+@RestController
+@RequestMapping("/customerapi")
 public class JSonProvidercontroller {
+	@GetMapping("/register")
+	public ResponseEntity<Customer> getcustomerinfo(){
+		Customer cust=new Customer(1012, "Shankar Reddy","Pedda Anberpet",1010.00f);
+		return new ResponseEntity<Customer>(cust,HttpStatus.OK);
+	}
 
 }
