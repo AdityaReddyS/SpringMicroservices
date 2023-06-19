c7a24ae162731f87787efefb9d65b9d9479b1d71
Dev2@gmail.com <shankarreddy@LAPTOP-HNPNI8HE>
2023-06-19 14:50:30 +0530

    Tl Added changes in Customer.java file

--- SpringRestProj1-JsonProvidingApp/src/main/java/com/shankar/model/Customer.java
+++ SpringRestProj1-JsonProvidingApp/src/main/java/com/shankar/model/Customer.java
@@ -1,5 +1,16 @@
 package com.shankar.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+	private int cid;
+	private String cname;
+	private String cadd;
+	private Float billAmt;
 
 }
